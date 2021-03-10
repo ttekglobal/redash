768ba77a88d3196c54b7af5c35ae60f418d4d5e7
Nguyễn Hồ Hải Triều <hohaitrieuntu@gmail.com>
2021-03-10 16:21:30 +0700

    [Dashboard API] Create Dashboard_Groups tables (#29)
    * [Dashboard API] Create Dashboard_Groups tables
    
    * [Dashboard API] Update Dashboard Groups
    
    * [Dashboard API] Update Dashboard Groups
    
    * [Dashboard API] Get more Dashboard prop
    
    Co-authored-by: Hải Triều Nguyễn Hồ <trieu@ltv.vn>

--- redash/models/__init__.py
+++ redash/models/__init__.py
@@ -1107,6 +1107,8 @@ class Dashboard(ChangeTrackingMixin, TimestampMixin, BelongsToOrgMixin, db.Model
         d = {
             "id": self.id,
             "name": self.name,
+            "tags": self.tags,
+            "type": 'google_analytics'
         }
 
         if all:
@@ -1231,7 +1233,7 @@ class Dashboard(ChangeTrackingMixin, TimestampMixin, BelongsToOrgMixin, db.Model
         db.session.commit()
 
     def update_group_permission(self, group, view_only):
-        dsg = DashboardGroup.query.filter(
+        dg = DashboardGroup.query.filter(
             DashboardGroup.group == group, DashboardGroup.dashboard == self
         ).one()
         dg.view_only = view_only
