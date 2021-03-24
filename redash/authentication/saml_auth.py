1ffcdcc738955447bfaaeefd709dfb96d0009790
Khiem Nguyen <khiem.nguyen@ttekglobal.com>
2021-03-24 10:16:33 +0700

    add log

--- redash/authentication/saml_auth.py
+++ redash/authentication/saml_auth.py
@@ -113,7 +113,7 @@ def idp_initiated(org_slug=None):
         return redirect(url_for("redash.login", org_slug=org_slug))
 
     print("------------------authn_response----------------\n")
-    print(authn_response.ava)
+    print(authn_response)
     # logger.info(authn_response)
     print("------------------end authn_response------------\n")
 
