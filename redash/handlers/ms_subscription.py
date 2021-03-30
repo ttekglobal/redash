0d2629e3a7ecb06c93375c6db345d5916348eba5
Hải Triều Nguyễn Hồ <trieu@ltv.vn>
2021-03-30 15:00:33 +0700

    [Syncing users]

--- redash/handlers/ms_subscription.py
+++ redash/handlers/ms_subscription.py
@@ -10,14 +10,9 @@ logger = logging.getLogger(__name__)
 @routes.route(org_scoped_rule("/subscriptioncallback"), methods=["POST"])
 def subscriptionCallback():
     try:
-        logger.info('Request Json')
-        logger.info(request.get_json)
-        logger.info('Arguments')
-        arguments = request.get_json(force=True)
-        logger.info(arguments)
-        token = arguments.get("validationToken", "")
+        token = request.args.get('validationToken')
         data = json_dumps({"validationToken": token})
         headers = {"Content-Type": "text/plain"}
         return make_response(token, 200, headers)
     except:
-        print('ERROR')
+        logger.error('ERROR')
